91a343f996aff2ec12bbb327d2860bff4c4e8089
Nikhil Balekundargi <nikhil.balekundargi@gmail.com>
2025-01-03 19:39:20 +0530

    tf code to crete s3 bucker and dynamo db table

---
+++ app/infra/backend/s3-dynamodb.tf
@@ -0,0 +1,89 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = var.region
+  profile = "root"
+}
+
+# Create S3 Bucket to store state file
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.bucket_name
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "terraform-state-bucket"
+    }
+  )
+}
+
+# Use aws_s3_bucket_server_side_encryption_configuration resource for server-side encryption
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_encryption" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# Use aws_s3_bucket_versioning resource for versioning
+resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Create bucket policy for restricted access
+resource "aws_s3_bucket_policy" "terraform_state_policy" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = "*",
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject"
+        ],
+        Resource = "${aws_s3_bucket.terraform_state.arn}/*",
+        Condition = {
+          StringEquals = {
+            "aws:PrincipalAccount" = var.account_id
+          }
+        }
+      }
+    ]
+  })
+}
+
+# Create DynamoDB table for state locking
+resource "aws_dynamodb_table" "quest_tf_state_lock" {
+  name         = "quest_tf_state_lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "terraform-lock-table"
+    }
+  )
+}
